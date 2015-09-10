1ad8761daf1d4ef664d8145ab8e9f7267cf075ca
jpwall <jessephilipwalling@gmail.com>
2015-09-09 19:05:52 -0700

    add twitch shell script that cycles through the streams I want to watch

---
+++ scripts/twitch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+livestreamer -p vlc twitch.tv/esl_csgo source
+livestreamer -p vlc twitch.tv/pgl source
+livestreamer -p vlc twitch.tv/counterpit source
+livestreamer -p vlc twitch.tv/faceittv source
+livestreamer -p vlc twitch.tv/warowl source
+livestreamer -p vlc twitch.tv/nickbunyun source
+livestreamer -p vlc twitch.tv/csboxr source
+livestreamer -p vlc twitch.tv/norrbria source
+livestreamer -p vlc twitch.tv/ak source
