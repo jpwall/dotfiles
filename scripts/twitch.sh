ffc59866d8992af2738be4ff98dd8d577e75a8c2
jpwall <jessephilipwalling@gmail.com>
2015-09-14 16:59:10 +0000

    add channel to twitch script

--- scripts/twitch.sh
+++ scripts/twitch.sh
@@ -4,6 +4,7 @@ livestreamer -p vlc twitch.tv/esl_csgo source
 livestreamer -p vlc twitch.tv/pgl source
 livestreamer -p vlc twitch.tv/counterpit source
 livestreamer -p vlc twitch.tv/faceittv source
+livestreamer -p vlc twitch.tv/ibuypower source
 livestreamer -p vlc twitch.tv/warowl source
 livestreamer -p vlc twitch.tv/nickbunyun source
 livestreamer -p vlc twitch.tv/csboxr source
